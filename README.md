3ed4c8a680c50ae820cb549f638e665458e75396
sudolink <sudolink@protonmail.com>
2022-09-26 23:29:23 +0200

    added more routes

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 Simple server that queries a Mysql database (vMangos WoW structure), and serves the data to the WoWArmory frontend.
 
+
+SETUP:
 Make an .env file in project root and make sure to define the following variables:
 DB_IP = dbip (localhost if running on same machine as the database)
 DB_PORT = dbport (not in use atm)
 DB_USER = dbuser
 DB_PW = password
+PORT = this server port (if you want to specify a port otherwise it'll run on 5000)
+
+npm run start
 
 I suggest making a new mysql user for the database, with only read permissions - you don't want sql injections, and I do barely and input-checking.
 
 Server accesses the 'characters' and 'mangos' databases and some of their tables.
 
 Needs access to these:
-characters database
- |--- characters          (need player's guid for equipment checking, class, and visuals),
- |--- character_inventory (checking only slots 0-18, i.e. what is equipped)
- |--- item_instance       (checking item instance for enchantments)
+characters database:
+characters          (need player's guid for equipment checking, class, and visuals),
+character_inventory (checking only slots 0-18, i.e. what is equipped)
+item_instance       (checking item instance for enchantments)
 
 mangos database:
- |--- item_template       (get base item info)
+item_template       (get base item info)
--- index.js
+++ index.js
@@ -6,11 +6,20 @@ import {createNewConn} from "./dbHelp.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//static folder for serving icons
+app.use(express.static("icons"))
+
 //middleware
 app.use(express.json()) //for raw json
 app.use(express.urlencoded({extended:false}))
 
-app.get("/getAllCharNames", (req,res) => {
+function reportRequest(req,res,next){
+    console.log(req.url);
+    next()
+}
+app.use(reportRequest);
+
+app.get("/api/getAllCharNames", (req,res) => {
     let dbConn = createNewConn();
     dbConn.connect();
     dbConn.query("SELECT name FROM characters", (err, rows, fields) => {
@@ -22,20 +31,24 @@ app.get("/getAllCharNames", (req,res) => {
     dbConn.end();    
 })
 
-app.get("/getchar", (req,res) => {
-    console.log(req.query.name);
+app.get("/api/getchar", (req,res) => {
+    // console.log(req.query.name);
     if(req.query.name != undefined) {
         let dbConn = createNewConn();
         dbConn.connect()
         const queryName = req.query.name;
         dbConn.query(`SELECT guid,name,race,class,gender,level,online FROM characters WHERE name="${queryName}"`, (err,rows,fields) => {
-            res.status(200).send(rows);
+            if(rows.length < 1){
+                res.status(404).send("No such character");
+            }else{
+                res.status(200).send(rows);
+            }
         })
         dbConn.end();
     }
 })
 
-app.get("/getchargear", (req,res)=> {
+app.get("/api/getchargear", (req,res)=> {
     console.log(req.query.guid)
     if(req.query.guid != undefined){
         let dbConn = createNewConn()
@@ -48,16 +61,29 @@ app.get("/getchargear", (req,res)=> {
     }
 })
 
-app.get("/getbaseitem", (req,res) => {
+app.get("/api/getbaseitem", (req,res) => {
     console.log(req.query.item_template);
     if(req.query.item_template != undefined){
-        let dbConn = createNewConn("mangos") //base item info is in this DB
+        let dbConn = createNewConn("mangos"); //base item info is in this DB
         const queryString = `SELECT * FROM item_template WHERE entry=${req.query.item_template}`;
-        dbConn.connect()
+        dbConn.connect();
         dbConn.query(queryString, (err,rows,fields)=>{
             res.status(200).send(rows);
         })
-        dbConn.end()
+        dbConn.end();
+    }
+})
+
+app.get("/api/getitemicon", (req,res) => {
+    console.log(req.query.display_id);
+    if(req.query.display_id != undefined){
+        let dbConn = createNewConn("mangos"); //icon info is in here
+        const queryString = `SELECT icon FROM item_display_info WHERE id=${req.query.display_id}`;
+        dbConn.connect();
+        dbConn.query(queryString, (err, rows, fields) => {
+            res.status(200).send(rows);
+        })
+        dbConn.end();
     }
 })
 
